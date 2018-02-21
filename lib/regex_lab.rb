0311f660f707b3aa2be6a4cb38cc13ff8318b860
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 23:03:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,9 +16,9 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z]\z\W/) == nil
-    false
+    return false
   else
-    true
+    return true
   end
 end
 
