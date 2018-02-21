514021e68e12b2179f19e0d1e3ea264d207ed3be
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 22:55:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,10 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if (text =~ /\A[A-Z]\z\W/) == nil
-    false
-  else
+  if (text =~ /\A[A-Z]\z\W/) == 0
     true
+  else
+    false
   end
 end
 
