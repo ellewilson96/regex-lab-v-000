5c97b9584b3a5e98469601495b2fc490f7e7be04
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 23:13:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone =~ (/\d{9}/i)
+  if phone =~ (//\b^([0-9]{10})$\b/)
     true
   else
     false
