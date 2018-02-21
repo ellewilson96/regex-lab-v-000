1e2866e56916c77ddd6d8ef239e24aeb88893f8f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 22:54:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\A[A-Z]\z\W/)
+  if text =~ /\A[A-Z]\z\W/) == nil
+    false
+  else
+    true
+  end
 end
 
 def valid_phone_number?(phone)
